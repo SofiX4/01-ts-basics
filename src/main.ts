fd518b76172e65ea6ea336df909795326cdadb33
Siela <boyko.sofania@gmail.com>
2026-01-15 18:50:57 +0100

    hw-ts1

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import "./style.css";
+import {} from "./task-1";
+import {} from "./task-2";
+import {} from "./task-3";
+import {} from "./task-4";
+import {} from "./task-5";
+import {} from "./task-6";
+import {} from "./task-7";
+import {} from "./task-8";
